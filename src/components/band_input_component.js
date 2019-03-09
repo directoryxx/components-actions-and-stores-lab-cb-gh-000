1846d39f2c74f6ea0e47eaa64c7c35ea789dcb4e
Anak Agung Angga Wijaya <anggawijaya4567@gmail.com>
2019-03-09 18:32:13 +0000

    Done.

--- src/components/band_input_component.js
+++ src/components/band_input_component.js
@@ -1,8 +1,35 @@
 import React, { Component } from 'react'
-class BandInputComponent extends Component {
-  render(
+import BandIndex  from './band_index_component';
+
+export default class BandInput extends React.Component {
+  constructor(props) {
+    super(props)
+    this.onSubmit = this.onSubmit.bind(this)
+  }
+
+  handleBandShow(){
+   return this.props.store.getState().map((band, i) => <BandIndex key={i} band={band} />)
+ }
+
+  onSubmit(event){
+    event.preventDefault();
+    this.props.store.dispatch({type: 'ADD_BAND', payload: {title: event.target.children[1].value}})
+    event.target.children[1].value = ''
+}
+
+  render(){
     return(
-      <div>hello</div>
+      <div>
+        <form onSubmit={this.onSubmit}>
+          <label>Enter Band: </label>
+          <input />
+          <button type="submit">Submit</button>
+        </form>
+        <h1>Bands</h1>
+            <ul>
+                {this.handleBandShow()}
+            </ul>
+      </div>
     )
-  )
+  }
 }
